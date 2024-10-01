ae88e9f652a2259fa2b4ce2b9becf5d1be23a80e
JIBAN JYOTI MOHANTY <jibanjyotimohanty315@gmail.com>
2024-10-01 18:24:24 +0530

    creating UsersDetails class inside service package

---
+++ src/main/java/com/jiban/Banking/services/UsersDetails.java
@@ -0,0 +1,28 @@
+package com.jiban.Banking.services;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UsersDetails implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAuthorities'");
+    }
+
+    @Override
+    public String getPassword() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPassword'");
+    }
+
+    @Override
+    public String getUsername() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getUsername'");
+    }
+    
+}
