31ca3d64c291fd00dfdce791e89ef1d19a079900
JIBAN JYOTI MOHANTY <jibanjyotimohanty315@gmail.com>
2024-10-01 17:42:37 +0530

    "Added JPA annotations for id field in Users entity"

--- src/main/java/com/jiban/Banking/entity/Users.java
+++ src/main/java/com/jiban/Banking/entity/Users.java
@@ -1,6 +1,12 @@
 package com.jiban.Banking.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String email;
