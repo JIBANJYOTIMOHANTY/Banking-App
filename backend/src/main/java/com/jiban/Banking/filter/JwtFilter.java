c724a24debc385b21ccd95c9187503433226ae83
JIBAN JYOTI MOHANTY <jibanjyotimohanty315@gmail.com>
2024-10-02 17:28:41 +0530

    Added missing `filterChain.doFilter(request, response);` call in `JwtFilter` class.

--- backend/src/main/java/com/jiban/Banking/filter/JwtFilter.java
+++ backend/src/main/java/com/jiban/Banking/filter/JwtFilter.java
@@ -50,7 +50,7 @@ public class JwtFilter extends OncePerRequestFilter {
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
-        
+        filterChain.doFilter(request, response);
     }
     
 }
