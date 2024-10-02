7c36196e4d3abdaff67240c9764d69ab76bd02c0
JIBAN JYOTI MOHANTY <jibanjyotimohanty315@gmail.com>
2024-10-02 18:06:03 +0530

    "Changed access modifier of passwordEncoder() method from private to public"

--- backend/src/main/java/com/jiban/Banking/config/SecurityConfig.java
+++ backend/src/main/java/com/jiban/Banking/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    private PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
