5a06b450b9a4adc337c9a496a4ba1a4a0ce2d8bc
JIBAN JYOTI MOHANTY <jibanjyotimohanty315@gmail.com>
2024-10-03 08:31:34 +0530

    "Added POST /signUp endpoint to BankController to create a new admin"

--- backend/src/main/java/com/jiban/Banking/controller/BankController.java
+++ backend/src/main/java/com/jiban/Banking/controller/BankController.java
@@ -3,8 +3,11 @@ package com.jiban.Banking.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jiban.Banking.entity.Admin;
 import com.jiban.Banking.services.AdminService;
 import com.jiban.Banking.services.JwtService;
 
@@ -23,4 +26,11 @@ public class BankController {
     public String welcome(){
         return "Welcome to Bank"   ;
     }
+
+    @PostMapping("/signUp")
+    public String addAdmin(@RequestBody Admin admin){
+        return adminService.addAdmin(admin);
+    }
+
+    
 }
